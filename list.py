4991db28d12d72cd38bffbe7ccb3d6a6a8263785
Calvin Chen <pingc@vmware.com>
2023-11-22 21:18:02 +0800

    python list

---
+++ list.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+print("there are {} fruits, they are: {}".format(len(fruits), fruits))
+
+fruits.append("orange")
+print("there are {} fruits, they are: {}".format(len(fruits), fruits))
+
+fruits.insert(1, "watermelon")
+print("there are {} fruits, they are: {}".format(len(fruits), fruits))
+
+fruits.remove("cherry")
+print("there are {} fruits, they are: {}".format(len(fruits), fruits))

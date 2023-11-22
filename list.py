e5bb2f6d43f223825133abb30314503dbba491dd
Calvin Chen <pingc@vmware.com>
2023-11-22 21:22:41 +0800

    python list

--- list.py
+++ list.py
@@ -9,3 +9,7 @@ print("there are {} fruits, they are: {}".format(len(fruits), fruits))
 
 fruits.remove("cherry")
 print("there are {} fruits, they are: {}".format(len(fruits), fruits))
+
+
+fruits.sort()
+print("there are {} fruits, they are: {}".format(len(fruits), fruits))

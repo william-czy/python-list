34578124c99c58fd24da66857292074e7bc8ae21
Calvin Chen <pingc@mac.local>
2023-11-23 00:08:48 +0800

    python list

--- list.py
+++ list.py
@@ -2,14 +2,13 @@ fruits = ["apple", "banana", "cherry"]
 print("there are {} fruits, they are: {}".format(len(fruits), fruits))
 
 fruits.append("orange")
-print("there are {} fruits, they are: {}".format(len(fruits), fruits))
+print("list.append - there are {} fruits, they are: {}".format(len(fruits), fruits))
 
 fruits.insert(1, "watermelon")
-print("there are {} fruits, they are: {}".format(len(fruits), fruits))
+print("list.insert - there are {} fruits, they are: {}".format(len(fruits), fruits))
 
 fruits.remove("cherry")
-print("there are {} fruits, they are: {}".format(len(fruits), fruits))
-
+print("list.remove - there are {} fruits, they are: {}".format(len(fruits), fruits))
 
 fruits.sort()
-print("there are {} fruits, they are: {}".format(len(fruits), fruits))
+print("list.sort   - there are {} fruits, they are: {}".format(len(fruits), fruits))

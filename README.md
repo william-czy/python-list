73622752fe8c7ffcc7e07e733b660c340bd267b2
Calvin Chen <pingc@vmware.com>
2023-11-23 20:32:49 +0800

    python list

--- README.md
+++ README.md
@@ -7,16 +7,18 @@
 - remove
 - pop
 - sort
+- reverse
 - clear
 
 ## 代码输出
 ```
 > python3 list.py
 there are 3 fruits, they are: ['banana', 'apple', 'cherry']
-list.append - there are 4 fruits, they are: ['banana', 'apple', 'cherry', 'orange']
-list.insert - there are 5 fruits, they are: ['banana', 'watermelon', 'apple', 'cherry', 'orange']
-list.remove - there are 4 fruits, they are: ['banana', 'watermelon', 'apple', 'orange']
-list.pop(1) - there are 3 fruits, they are: ['banana', 'apple', 'orange']
-list.sort   - there are 3 fruits, they are: ['apple', 'banana', 'orange']
-list.clear  - there are 0 fruits, they are: []
+list.append  - there are 4 fruits, they are: ['banana', 'apple', 'cherry', 'orange']
+list.insert  - there are 5 fruits, they are: ['banana', 'watermelon', 'apple', 'cherry', 'orange']
+list.remove  - there are 4 fruits, they are: ['banana', 'watermelon', 'apple', 'orange']
+list.pop(1)  - there are 3 fruits, they are: ['banana', 'apple', 'orange']
+list.sort    - there are 3 fruits, they are: ['apple', 'banana', 'orange']
+list.reverse - there are 3 fruits, they are: ['orange', 'banana', 'apple']
+list.clear   - there are 0 fruits, they are: []
 ```
--- list.py
+++ list.py
@@ -2,19 +2,22 @@ fruits = ["banana", "apple", "cherry"]
 print("there are {} fruits, they are: {}".format(len(fruits), fruits))
 
 fruits.append("orange")
-print("list.append - there are {} fruits, they are: {}".format(len(fruits), fruits))
+print("list.append  - there are {} fruits, they are: {}".format(len(fruits), fruits))
 
 fruits.insert(1, "watermelon")
-print("list.insert - there are {} fruits, they are: {}".format(len(fruits), fruits))
+print("list.insert  - there are {} fruits, they are: {}".format(len(fruits), fruits))
 
 fruits.remove("cherry")
-print("list.remove - there are {} fruits, they are: {}".format(len(fruits), fruits))
+print("list.remove  - there are {} fruits, they are: {}".format(len(fruits), fruits))
 
 fruits.pop(1)
-print("list.pop(1) - there are {} fruits, they are: {}".format(len(fruits), fruits))
+print("list.pop(1)  - there are {} fruits, they are: {}".format(len(fruits), fruits))
 
 fruits.sort()
-print("list.sort   - there are {} fruits, they are: {}".format(len(fruits), fruits))
+print("list.sort    - there are {} fruits, they are: {}".format(len(fruits), fruits))
+
+fruits.reverse()
+print("list.reverse - there are {} fruits, they are: {}".format(len(fruits), fruits))
 
 fruits.clear()
-print("list.clear  - there are {} fruits, they are: {}".format(len(fruits), fruits))
+print("list.clear   - there are {} fruits, they are: {}".format(len(fruits), fruits))
